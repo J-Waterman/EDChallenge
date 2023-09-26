fe05e3b39c2f868be0568702256ee13ebd86eb92
J-Waterman <joshwaterman42@gmail.com>
2023-09-26 12:13:17 -0500

    order generator

--- scripts/order_generator.py
+++ scripts/order_generator.py
@@ -5,15 +5,15 @@ def generate_orders(num_orders=10):
     orders = []
     for _ in range(num_orders):
         order = {
-            "client_id": random.randint(1, 140),  # Example range from 1 to 100
+            "client_id": random.randint(1, 140),
             "event_id": random.randint(1, 140),
-            "order_id": random.randint(1, 100000)  # Example range from 1 to 100000
+            "order_id": random.randint(1, 100000)
         }
         orders.append(order)
     return orders
 
 def main():
-    num_orders_to_generate = 222  # Example: generate 10 orders
+    num_orders_to_generate = 222
     orders = generate_orders(num_orders_to_generate)
     print(json.dumps(orders, indent=4))
 
